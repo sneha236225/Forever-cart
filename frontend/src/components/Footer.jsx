8287570360830b97fb41b4720efa957a03e68472
Sneha Agrawal <agrawalsneha601@gmail.com>
2025-05-26 13:50:36 +0530

    second commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            Everything you need is just a click away. With top deals, trusted brands, and fast doorstep delivery, shopping has never been this simple or satisfying.Big savings bring even bigger smiles! Enjoy exclusive discounts and exciting daily offers — because you deserve the best products at the best prices.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-212-456-7890</li>
+                <li>contact@foreveryou.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
