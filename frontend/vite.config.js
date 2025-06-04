b38b2b9e6298a423abe9a40da019cea8c9639d61
Sneha Agrawal <agrawalsneha601@gmail.com>
2025-06-04 21:45:00 +0530

    changing env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  server: {
+    port: 5173
+  },
+  define: {
+    'process.env': {} // Optional, avoids some Vite warnings
+  }
 })
